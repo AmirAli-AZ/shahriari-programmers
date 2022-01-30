7f0a4631864cbe5e15bcbff48e1fb685709e79cc
AmirAli-AZ <azimi6370@gmail.com>
2022-01-30 13:15:54 +0330

    download page added

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+window.onscroll = function() {
+  pageScroll()
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function pageScroll() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
